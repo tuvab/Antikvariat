47820fa06e47f11986ce67c0efd681d107476345
tuvab <>
2020-10-22 16:27:33 +0200

    Commit

---
+++ Prov-Antikvariat/Customers.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Prov_Antikvariat
+{
+    public class Customers
+    {
+    }
+}
---
+++ Prov-Antikvariat/Items.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System;
+
+namespace Prov_Antikvariat
+{
+    public class Items
+    {
+        public int price;
+        private string name; 
+        private int rarity;
+        private string category;
+        private int actualValue;
+        private bool cursed;
+        private Random generator = new Random();
+
+        public void Book()
+        {
+            List<string> titles = new List<string>() {"a good book", "a great book"};
+            List<string> category = new List<string>() {"Horror", "Fantasy", "Kittens", "More Kittens"};
+
+            actualValue = generator.Next(1,101);
+            rarity = generator.Next(1,11);
+            //name = generator.Next(1, titles.length);
+            int determineCursed = generator.Next(1,3);
+            if (determineCursed == 1)
+            {
+                cursed = true;
+            }
+            else
+            {
+                cursed = false;
+            }
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(actualValue);
+        }
+
+        public int Evaluate()
+        {
+            int correctPrice;
+            actualValue * rarity = correctPrice;
+            int priceDifference = generator.Next(50,151);
+
+            return 0;
+        }
+
+        public string GetCategory()
+        {
+
+            return "";
+        }
+
+    }
+}
