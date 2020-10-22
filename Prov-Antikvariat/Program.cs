24d18a85680c4f010c20ede27dd6c351b22e0fa1
tuvab <>
2020-10-22 16:20:48 +0200

    Commit

--- Prov-Antikvariat/Program.cs
+++ Prov-Antikvariat/Program.cs
@@ -6,7 +6,18 @@ namespace Prov_Antikvariat
     {
         static void Main(string[] args)
         {
-             
+            Items book1 = new Items();
+
+            /* string fullAnswer;
+            string answer;
+
+            fullAnswer = Console.ReadLine();
+            answer = fullAnswer.Trim();
+
+            if ((String.Compare(answer, "book", true) == 0) || (String.Compare(answer, "bok", true) == 0))
+            {
+                
+            } */
         }
     }
 }
